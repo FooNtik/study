de7efd4d78c2ce3586ff54fe535cb76f62fb8a58
Ruslan Molodik <ruslan.molodik@form.com>
2025-10-17 10:29:23 +0200

    Add strings examples

---
+++ StringsTests/StringsTokens.java
@@ -0,0 +1,23 @@
+package StringsTests;
+
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class StringsTokens {
+  public static void main(String[] args) {
+    Scanner scan = new Scanner(System.in);
+    String s = scan.nextLine();
+    scan.close();
+
+    String[] tokens = s.split("[^A-Za-z]+");
+
+    tokens = Arrays.stream(tokens)
+        .filter(token -> !token.isEmpty())
+        .toArray(String[]::new);
+
+    System.out.println(tokens.length);
+    for (String token : tokens) {
+      System.out.println(token);
+    }
+  }
+}
